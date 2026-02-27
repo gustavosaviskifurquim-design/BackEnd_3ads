a818afe17959bb7c6f1298fb8490e42ca7b5f8de
gustavosaviskifurquim-design <gustavo.saviski.furquim@escola.pr.gov.br>
2026-02-27 13:59:18 +0000

    codigo if else e modificação para ternário

---
+++ condicionais/ternario01.js
@@ -0,0 +1,15 @@
+//ternário é ima forma simplificada de escrever 
+// if else
+
+let solteiro = true
+
+if (solteiro == true) {
+  console.log("ta na pista")
+} else {
+  console.log("se liga tenho compromisso!")
+}
+//expressão ternária 
+const estadocivil = solteiro == true
+? 'ta na pista!' : 'se liga tenho compromisso '
+  console.log(estadocivil)
+
---
+++ variavel/varievel-pessoas.js
@@ -0,0 +1,53 @@
+// var, let, const
+//valores constantes e variáveis
+//operadores: +, -, / , %
+
+let nome = "petter"       //string
+let sobrenome = "saviski furquim"    //string
+const cpf = "899.888.999.99"      //string
+const rg = "181818"            //string
+let idade = 39
+let nota = 8.5 + 1.5     //number
+let solteiro = true      //boolean
+
+console. log(nome)
+//faça o mesmo para as outra variaveis
+
+console. log (typeof nome)
+//faça o mesmo as para outras
+
+console. log(sobrenome)
+//faça o mesmo para as outra variaveis
+
+console. log (typeof sobrenome)
+//faça o mesmo as para outras 
+
+console. log(cpf)
+//faça o mesmo para as outra variaveis
+
+console. log (typeof cpf)
+//faça o mesmo as para outras 
+
+console. log(rg)
+//faça o mesmo para as outra variaveis
+
+console. log (typeof rg)
+//faça o mesmo as para outras
+
+console. log(rg)
+//faça o mesmo para as outra variaveis
+
+console. log (typeof rg)
+//faça o mesmo as para outras
+
+console. log(nota)
+//faça o mesmo para as outra variaveis
+
+console. log (typeof nota)
+//faça o mesmo as para outras 
+
+console. log(solteiro)
+//faça o mesmo para as outra variaveis
+
+console. log (typeof solteiro)
+//faça o mesmo as para outras 
