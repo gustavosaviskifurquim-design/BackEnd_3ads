d05c73be52088ebae3fc7da43f2fc96380fce70a
gustavosaviskifurquim-design <gustavo.saviski.furquim@escola.pr.gov.br>
2026-03-05 14:15:56 +0000

    finalizar if else e introdução aos Arrays

---
+++ arrays/array-time.js
@@ -0,0 +1,13 @@
+// um array é um grupo grande indexado de valores
+//podem ser homegẽneos / heterogẽneos
+// seus indice vão de "0" ... "infinito"
+//possuem funções nativas (prontas)
+//podem ser iterados e checados 
+
+
+const time = ['peter' , 'pan' , true, 56]
+         // indice 0      1     2     3
+
+console.log(time)
+console.log(time[1])
+console.log(time[32])
---
+++ condicionais/if-login.js
@@ -0,0 +1,12 @@
+// vereficar se login e senha
+// comrespondem ao mesmo tempo (&&)
+
+    const login = 'superman'
+    const senha = 'segredo123'
+
+    if (login == 'superman' && senha =='segredo123' ) {
+    console.log("acesso liberado")
+    }else{
+        console.warn('verefique seus dados')
+        }
+  
